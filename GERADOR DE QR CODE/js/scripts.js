27075a1f06d92919ec62a8ee95bb27382ca3d40a
Marcellyz <146850083+Marcellyz@users.noreply.github.com>
2024-01-15 19:57:07 -0300

    Update scripts.js

--- GERADOR DE QR CODE/js/scripts.js
+++ GERADOR DE QR CODE/js/scripts.js
@@ -1,18 +1,21 @@
+//SELEÇÃO DE ELEMENTOS
 const container = document.querySelector(".container");
 const qrCodeBtn = document.querySelector("#qr-form button");
 
 const qrCodeInput = document.querySelector("#qr-form input");
 const qrCodeImg = document.querySelector("#qr-code img")
-// Eventos
 
  function generateQrCode() {
+  //função para resgatar o valor do input
     const qrCodeInputValue = qrCodeInput.value;
     if(!qrCodeInputValue) return;
 
     qrCodeBtn.innerText = "Gerando código...";
 
+  //API
     qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
 
+  // Eventos
     qrCodeImg.addEventListener("load", () => {
         container.classList.add("active");
         qrCodeBtn.innerText = "Código criado";
